88c246cca084db5d076c0825faab910fa23aa690
smink112 <smink95@naver.com>
2023-11-13 13:24:27 +0900

    아마 되겠징 ㅎㅎ

--- ccachi/src/main/java/com/ssafit/ccachi/CcachiApplication.java
+++ ccachi/src/main/java/com/ssafit/ccachi/CcachiApplication.java
@@ -2,12 +2,18 @@ package com.ssafit.ccachi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CcachiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CcachiApplication.class, args);
 	}
-
+	@RequestMapping("/")
+	public String home(){
+		return "Hello world Spring";
+	}
 }
